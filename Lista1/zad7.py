220340e80bc71085a02af1e2cf62e1e4eddfc807
Marcin <marcinsz9045@gmail.com>
2020-03-08 21:13:08 +0100

    Skonczona lista 1

--- Lista1/zad7.py
+++ Lista1/zad7.py
@@ -5,12 +5,12 @@ pattern = "a**a******"
 
 def matcher(pattern, list):
     result = []
-    dict = []
+    dict = {}
 
     for i in range(0, len(pattern)):
         if pattern[i].isalpha():
-            dict.append(i)
-    
+            dict[i] = pattern[i]
+            
     for word in list:
         for indeks in dict:
             if word[indeks] != pattern[indeks]:
--- Lista1/zad8.py
+++ Lista1/zad8.py
@@ -1,1 +1,24 @@
 #Program konwertujacy liczby rzymskie na arabskie
+
+
+def converter(num):
+    symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V','IV','I']
+    values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    result = 0
+    i =0
+    s =0
+    while s < len(num):
+        if num[s] == symbols[i]:
+            result += values[i]
+            s += 1
+        elif (len(num) - s >= 2) and (num[s] + num[s+1]) == symbols[i+1]:
+                i +=1
+                result += values[i]
+                i +=1
+                s += 2
+        else:
+            i += 2
+    return result     
+
+number = input("Podaj liczbe rzymska: ")
+print(converter(number))
